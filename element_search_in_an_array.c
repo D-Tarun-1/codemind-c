921b14bebd7025f0a9467ab953c2a5aaa443d37f
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2022-12-10 11:51:10 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int z,c=0;     scanf("%d",&z);     for(i=0;i<n;i++)     {         if(a[i]==z)         {         c++;         break;     }     }     if(c!=0)     {         printf("True");     }     else     {         printf("False");     } }

--- element_search_in_an_array.c
+++ element_search_in_an_array.c
@@ -3,7 +3,7 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i,s=0;
+    int a[n],i;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -14,18 +14,16 @@ int main()
     {
         if(a[i]==z)
         {
-            c++;
-            break;
-        }
+        c++;
+        break;
+    }
     }
     if(c!=0)
     {
-    printf("True");
+        printf("True");
     }
     else
     {
-    printf("False");
+        printf("False");
     }
 }
-    
-    
