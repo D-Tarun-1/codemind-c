b1e3b8099f1375c8d2e3be28d009980ac9865bc2
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2022-12-29 11:08:06 +0530

    #include<stdio.h> int main() {     int n,k,m,d;     scanf("%d%d%d",&n,&k,&m);     d=k*m;     if(n%d==0)     printf("%d",n/d);     else if(n%d!=0)     printf("%d",(n/d)+1); }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m,d;
+    scanf("%d%d%d",&n,&k,&m);
+    d=k*m;
+    if(n%d==0)
+    printf("%d",n/d);
+    else if(n%d!=0)
+    printf("%d",(n/d)+1);
+}
