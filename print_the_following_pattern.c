3117cfa68bd2cacf95faa319df433902e1ab916d
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-01-20 10:18:19 +0530

    #include<stdio.h> int main() {   int n,i,j;   scanf("%d",&n);   for(i=1;i<=n;i++)   {     for(j=n;j>0;j--)     {         printf("%d ",j);     }     printf(" ");   } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    scanf("%d",&n);
-    for(i=n;i>=1;i--)
+  int n,i,j;
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=n;j>0;j--)
     {
-        for(j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
-        printf("
-");
+        printf("%d ",j);
     }
+    printf("
+");
+  }
 }
